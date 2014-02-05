d9bf4f51bb8b47de6aa8e3ed3444b35ce221655b
Brandon Horst <brandonhorst@gmail.com>
2014-02-05 15:07:50 -0500

    Allow reading of settings

--- InterSystems.py
+++ InterSystems.py
@@ -5,22 +5,6 @@ import subprocess
 import sublime_plugin
 import sublime
 
-def default_instance_name_get():
-    ccontrol = subprocess.Popen(['ccontrol', 'default'],stdout=subprocess.PIPE)
-    stdout = ccontrol.communicate()[0]
-    return stdout.decode('UTF-8').split('\n')[0]
-
-def path_get():
-    instanceName = default_instance_name_get()
-    ccontrol = subprocess.Popen(['ccontrol', 'qlist'],stdout=subprocess.PIPE)
-    stdout = ccontrol.communicate()[0]
-    instanceStrings = stdout.decode('UTF-8').split('\n')
-    for instanceString in instanceStrings:
-        instanceArray = instanceString.split('^')
-        if instanceName.upper() == instanceArray[0]:
-            return instanceArray[1]
-    raise
-
 def settings_get(name, default=None):
     # load up the plugin settings
     plugin_settings = sublime.load_settings('InterSystemsCache.sublime-settings')
@@ -39,10 +23,29 @@ def settings_get(name, default=None):
     return setting
 
 def call_cstud(*args,stdin=None):
+    instanceName = settings_get('current-server')
+    servers = settings_get('servers',{})
+    server = servers.get(instanceName)
+    if not server:
+        sublime.error_message("Cache Servers configured improperly")
+        return None
+    defaultArgs = [sys.executable, '{0}/cstud/cstud.py'.format(os.path.dirname(os.path.realpath(__file__))),
+                   '-U', server['username'],
+                   '-P', server['password'],
+                   '-H', server['host'],
+                   '-S', server['super-server-port'],
+                   '-N', server['namespace'],
+                   '-W', server['web-server-port']]
+
     pipeStdin = subprocess.PIPE if stdin else None
     communicateArgs = [bytes(stdin,"UTF-8")] if stdin else []
-    cstud = subprocess.Popen([sys.executable, '{0}/cstud/cstud.py'.format(os.path.dirname(os.path.realpath(__file__)))] + list(args), stdout=subprocess.PIPE, stdin=pipeStdin)
-    return cstud.communicate(*communicateArgs)[0].decode('UTF-8')
+    cstud = subprocess.Popen(defaultArgs + list(args), stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=pipeStdin)
+    stdout,stderr = cstud.communicate(*communicateArgs)
+    print(stderr)
+    return stdout.decode('UTF-8')
+
+def path_get():
+    return call_cstud("info","-l")
 
 bindingsPath = path_get()
 os.environ['PATH'] = "{0}:{1}/bin".format(os.environ['PATH'],bindingsPath)
