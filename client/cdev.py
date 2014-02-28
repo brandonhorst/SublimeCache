a96cfa9c4f92b0496c0ab0b94d85e35ba70baa1a
Brandon Horst <brandonhorst@gmail.com>
2014-02-27 23:42:27 -0500

    Wrote some unit tests and they pass

--- client/cdev.py
+++ client/cdev.py
@@ -4,6 +4,7 @@ import base64
 import json
 import os
 import sys
+import urllib.error
 import urllib.request
 import urllib.parse
 
@@ -14,8 +15,29 @@ class CDevException(Exception):
     def __str__(self):
         return "CDev Exception #{0}: {1}".format(self.code,self.desc)
 
+class Namespace:
+    def __init__(self, obj):
+        self.id = obj['id']
+        self.name = obj['name']
+        self.filesurl = obj['files']
+
+class File:
+    def __init__(self, obj):
+        self.id = obj['id']
+        self.name = obj['name']
+        self.content = obj.get('content')
+        self.generatedurl = obj.get('generatedfiles')
+        self.url = obj.get('url')
+
+class FileOperation:
+    def __init__(self, obj):
+        self.success = obj['success']
+        self.errors = obj.get('errors')
+        if 'file' in obj:
+            self.file = File(obj['file'])
+
 class CacheInstance:
-    def __init__(self, host, super_server_port, web_server_port, username, password):
+    def __init__(self, host, web_server_port, username, password):
         self.host = host
         self.port = web_server_port
         self.username = username
@@ -27,34 +49,75 @@ class CacheInstance:
         return "http://{0}:{1}".format(self.host,self.port)
 
     def get_namespaces(self):
-        return self._request(self.ROOTURL)
+        """
+        returns:
+            [ Namespace ]
+        """
+        namespaces = self._request(self.ROOTURL)
+        return [Namespace(namespace) for namespace in namespaces]
 
-    def get_namespace(self,name):
-        for namespace in self.get_namespaces():
-            if namespace['name'] == name:
-                return self._request(namespace['id'])
+    # def get_namespace(self,name):
+    #     """
+    #     accepts:
+    #         name: str #Numan-readable name. All caps.
+    #     returns: Namespace
+    #         {
+    #             id:    #Namespace URL
+    #             name:  #Human-readable name. All caps.
+    #             files: #URL to download files
+
+    #         }
+    #     """
+    #     for namespace in self.get_namespaces():
+    #         if namespace.name == name:
+    #             return self._request(namespace['id'])
 
     def get_files(self,namespace):
-        return self._request(namespace['files'])
+        """ returns: [ File ] 'content' key not included """
+        files = self._request(namespace.filesurl)
+        return [File(file) for file in files]
 
     def get_file(self, file):
-        return self._request(file['id'])
+        """ accepts: File 'content' key not required
+            returns: File """
+        result = self._request(file.id)
+        return File(result)
+
 
     def put_file(self, file):
-        return self._request(file['id'], method="PUT", data=file)
+        """ accepts: File """
+        result = self._request(file.id, method="PUT", data=file)
+        return FileOperation(result)
 
     def add_file(self, namespace, filename, filecontent):
+        """ accepts:
+                namespace:   Namespace
+                filename:    str # file name with lowercase extension
+                filecontent: str # content (UDL or Routine). Line endings will be automatically converted to \r\n. Class name must match filename.
+            returns: File """
         data = { 'name': filename, 'content': filecontent }
-        return self._request(namespace['files'], "PUT", data)
+        result = self._request(namespace.filesurl, "PUT", data)
+        return FileOperation(result)
 
     def compile_file(self, file, spec=""):
+        """ accepts:
+                file: File
+                spec: %SYSTEM.OBJ flags and compilers. Defaults are defined by the server.
+            returns: FileOperation """
         command = { 'action': 'compile', 'spec': spec }
-        return self._request(file['id'], method="POST", data=command)
+        result = self._request(file.id, method="POST", data=command)
+        return FileOperation(result)
 
     def get_generated_files(self,file):
-        return self.request(file['generated'])
+        """ accepts: File
+            returns: [ File ] 'content' key not included """
+        files = self._request(file.generatedurl)
+        return [File(file) for file in files]
+
 
     def _request(self, url, method="GET", data=None):
+        if data and hasattr(data,'__dict__'):
+            data = data.__dict__
         requestData = json.dumps(data).encode() if data else None
         requestHeaders = {'Content-Type':'application/json'} if data else {}
         requestUrl = self.url_prefix + url
@@ -70,8 +133,13 @@ class CacheInstance:
             method = method
         )
         
-        response = urllib.request.urlopen(request)
-        return json.loads(response.read().decode())
+        try:
+            response = urllib.request.urlopen(request)
+        except urllib.error.URLError as e:
+            print("Error Response: {0}".format(e.read()))
+            return None
+        result = response.read().decode()
+        return json.loads(result)
 
 # if __name__=="__main__":
 #     i = CacheInstance("172.16.196.221", "57772", "USER", "_SYSTEM", "SYS")
---
+++ client/test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import unittest
+import cdev
+
+
+class TestCDEVServer(unittest.TestCase):
+    def setUp(self):
+        self.instance = cdev.CacheInstance('172.16.197.130', '57772', '_SYSTEM', 'SYS')
+    def test_classes_and_routines(self):
+        namespaces = self.instance.get_namespaces()
+        self.assertIn('SAMPLES', [namespace.name for namespace in namespaces]) 
+
+        samples = [namespace for namespace in namespaces if namespace.name == 'SAMPLES'][0]
+        files = self.instance.get_files(samples)
+        self.assertIn('Sample.Person.cls', [file.name for file in files])
+        self.assertIn('LDAP.mac', [file.name for file in files])
+
+        personfile = [file for file in files if file.name == 'Sample.Person.cls'][0]
+        person = self.instance.get_file(personfile)
+        self.assertIn('Class Sample.Person', person.content)
+
+        ldapfile = [file for file in files if file.name == 'LDAP.mac'][0]
+        ldap = self.instance.get_file(ldapfile)
+        self.assertIn('LDAP', ldap.content)
+
+        person.content = '///modified by cdev\\r\\n{0}'.format(person.content)
+        putmodifiedpersonrequest = self.instance.put_file(person)
+        self.assertTrue(putmodifiedpersonrequest.success)
+
+        ldap.content = '///modified by cdev\\r\\n{0}'.format(ldap.content)
+        putmodifiedldaprequest = self.instance.put_file(ldap)
+        self.assertTrue(putmodifiedldaprequest.success)
+
+        newpersoncontent = person.content.replace('Sample.Person','Sample.CDEVPerson').replace('Stored_Procedure_Test','CDEV_Stored_Procedure_Test').replace('SP_Sample_By_Name','CDEV_Sample_By_Name')
+        newpersonname = 'Sample.CDEVPerson.cls'
+        newpersonresult = self.instance.add_file(samples, newpersonname, newpersoncontent)
+        self.assertTrue(newpersonresult.success)
+
+        newldapcontent = ldap.content
+        newldapname = 'CDEVLDAP.mac'
+        newldapresult = self.instance.add_file(samples, newldapname, newldapcontent)
+        self.assertTrue(newldapresult.success)
+
+        compilationresult = self.instance.compile_file(newpersonresult.file, 'ck')
+        self.assertTrue(compilationresult.success)
+
+        generatedfiles = self.instance.get_generated_files(compilationresult.file)
+        self.assertIn('Sample.CDEVPerson.1.int', [file.name for file in generatedfiles])
+
+        intfile = [file for file in generatedfiles if file.name == 'Sample.CDEVPerson.1.int'][0]
+        int = self.instance.get_file(intfile)
+        self.assertIn('Sample.CDEVPerson.1', int.content)
+
+if __name__=='__main__':
+    unittest.main()
